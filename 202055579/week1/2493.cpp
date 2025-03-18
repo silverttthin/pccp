1874c50b61f8e3b4f7fc390d5452e437f03bd3d3
Lee Si Woong <silverttthin@naver.com>
2025-03-18 23:51:27 +0900

    2493: 탑

---
+++ 202055579/week1/2493.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, arr[500003];
+stack<pair<int, int>> q;
+
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0);
+    cin>>n;
+    q.push({100000005, 0}); // ㅈㄴ 큰탑 넣어두기 for empty case 배제
+    q.push({arr[1], 1});
+    
+    for(int i=1; i<=n; i++){
+        int height;
+        cin>>height;
+        while(q.top().first < height) q.pop();
+        cout<<q.top().second<<" "; // 그냥 실시간으로 답 출력해가기
+        q.push({height, i});
+        }
+    }
+
