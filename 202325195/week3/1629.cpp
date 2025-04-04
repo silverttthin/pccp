071e7e8e1493d768f2615a02cb2e9bf27b7896b8
xz0901 <163363748+xz0901@users.noreply.github.com>
2025-04-04 14:34:01 +0900

    Update 1629.cpp

--- 202325195/week3/1629.cpp
+++ 202325195/week3/1629.cpp
@@ -12,7 +12,7 @@ typedef long long ll;
 
 ll power(ll a, ll b, ll c){
 	if(b==0) return 1; //지수가 0일때 결과 1 
-	if(b==1) return a % c; // 지수가 1일때는 그냥  나누기
+	if(b==1) return a % c; // 지수가 1일때는 그냥  
 	
 	//지수가 짝수일 때.. 
 	ll h = power(a, b/2, c); //지수를 반으로 나눔
@@ -25,5 +25,13 @@ ll power(ll a, ll b, ll c){
 	
 	
 	return result;
+	
+}
+
+
+int main(){
+	ll A, B, C;
+	cin >> A >> B >> C;
+	cout << power(A, B, C) << endl;
 	
 }
