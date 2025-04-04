f1d26ac7d662e751d7f574320f04d674337191ad
xz0901 <163363748+xz0901@users.noreply.github.com>
2025-04-04 13:52:35 +0900

    Create 1629.cpp

---
+++ 202325195/week3/1629.cpp
@@ -0,0 +1,29 @@
+/*1629, 곱셈, BOJ
+ 0ms
+ 27m*/
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+
+ll power(ll a, ll b, ll c){
+	if(b==0) return 1; //지수가 0일때 결과 1 
+	if(b==1) return a % c; // 지수가 1일때는 그냥  나누기
+	
+	//지수가 짝수일 때.. 
+	ll h = power(a, b/2, c); //지수를 반으로 나눔
+	ll result = (h * h) % c;
+	
+	//지수가 홀수일 때.. 
+	if(b%2 ==1){
+		result = (result * a)%c; 
+	}
+	
+	
+	return result;
+	
+}
