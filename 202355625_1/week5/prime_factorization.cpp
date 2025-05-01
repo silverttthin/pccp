b6cfb68e876eb6430175181db1bde573fcafacaf
SOYOUNG0625 <127066020+SOYOUNG0625@users.noreply.github.com>
2025-05-01 14:23:51 +0900

    Create prime_factorization.cpp

---
+++ 202355625_1/week5/prime_factorization.cpp
@@ -0,0 +1,18 @@
+//11653 / 소인수분해 / baekjoon
+//0ms, 10m
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 2; i * i <= n; i++) {
+        while (n % i == 0) {
+            cout << i << '\n';
+            n /= i;
+        }
+    }
+    if (n > 1) {
+        cout << n << '\n';
+    }
+    return 0;
+}
